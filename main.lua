bdb8e729fa03202ef5d03ddd0653164fd25ec3c2
24vaztoodix24-lang <24vaztoodix24@gmail.com>
2026-09-12 21:48:28 +0200

    main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/24vaztoodix24-lang/Script-roblox-Vaztoodix-RB/refs/heads/main/part1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/24vaztoodix24-lang/Script-roblox-Vaztoodix-RB/refs/heads/main/part2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/24vaztoodix24-lang/Script-roblox-Vaztoodix-RB/refs/heads/main/part3.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/24vaztoodix24-lang/Script-roblox-Vaztoodix-RB/refs/heads/main/part4.lua"))()
